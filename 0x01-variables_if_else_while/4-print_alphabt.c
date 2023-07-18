0d76dfb1576d0c24c3f83112cfeaa07f9a1952a5
Samuel Monye <toptechbro@gmail.com>
2023-07-17 19:55:21 -0700

    Task 5

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+/**
+ * main - Entry point of the program.
+ *
+ * Description: Prints the English alphabet in lowercase,
+ * excluding the letters 'e' and 'q'.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints all single-digit numbers in base 10, starting from 0.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
