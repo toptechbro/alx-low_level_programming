8a84c4dad68202c03d6a04713f1e2b3d1611746b
Samuel Monye <toptechbro@gmail.com>
2023-07-22 09:50:14 -0700

    update

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -24,11 +24,14 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else
+	else if (b > a && c > b)
 	{
 		largest = c;
 	}
-
+	else 
+	{
+		largest = b;
+	}
 	return (largest);
 }
 
