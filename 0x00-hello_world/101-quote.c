ed05fb3c5fd5b519d00fad1af5aa2e05067a8f61
Samuel Monye <toptechbro@yahoo.com>
2023-08-24 15:14:32 -0700

    Prints a quote with special character

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints a quote
+ *
+ * Return: returns 0 always
+ */
+
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
+
+
+
