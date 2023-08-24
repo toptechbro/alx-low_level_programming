d622d43d88fd74f3e795c13ec0a7fa6eeb5f4876
Samuel Monye <toptechbro@yahoo.com>
2023-08-24 11:20:05 -0700

    done

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+
+/**
+ * print_first - prints a sentence befor main func
+ * Return : void
+ */
+
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
