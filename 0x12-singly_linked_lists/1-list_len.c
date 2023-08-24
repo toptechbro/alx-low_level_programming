c65fb6d0c72ae9c933b1df383a788f44d561d1b8
Samuel Monye <toptechbro@yahoo.com>
2023-08-24 10:59:37 -0700

    Task 3

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
