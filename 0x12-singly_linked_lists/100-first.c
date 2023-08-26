c1d7315c2bce8973b06901b350ffbb22601ceeda
Samuel Monye <toptechbro@yahoo.com>
2023-08-25 17:21:54 -0700

    Task 3 correction

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,7 @@
  * Return : void
  */
 
-void print_first(void) __attribute__ ((constructor));
+void print_first(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
