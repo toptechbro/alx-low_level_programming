afcfd32414a42fc9370b4a42fc605c55010b84dd
Samuel Monye <toptechbro@yahoo.com>
2023-08-28 14:15:19 -0700

    Print list and list lenght

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - function that returns the number of element in a linked list
+ * @h: pointer to head first node
+ *
+ * Return: return the number of the elements (count)
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 1;
+
+	if (h == NULL)
+		return (0);
+	while (h->next != 0)
+	{
+		h = h->next;
+		count += 1;
+	}
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This directory contains the '0x13. C - More' project
