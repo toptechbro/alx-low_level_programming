4b0644d240a1edeee5dbcafcd2630abf0f08234f
Samuel Monye <toptechbro@yahoo.com>
2023-08-30 17:13:35 -0700

    0x0C. C - More malloc, free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -39,7 +39,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	ptr = malloc(nmemb * size);
-	if (ptr == NULLL)
+	if (ptr == NULL)
 		return (NULL);
 	_memset(ptr, 0, nmemb * size);
 	return (ptr);
