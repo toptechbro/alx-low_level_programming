6aafb1fee53b6be4eabd3367432bb4d2d3874775
Samuel Monye <toptechbro@yahoo.com>
2024-06-30 09:38:13 -0700

    Add: Task 9 file

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,10 +11,15 @@ int main(void)
 
 	for (a = 0; a < 10; a++)
 	{
-		putchar(a + '0');
-		putchar(',');
-		putchar(' ');
+		if (a < 9)
+		{
+			putchar(a + '0');
+			putchar(',');
+			putchar(' ');
+		} else
+		{
+			putchar(a + '0');
+		}
 	}
-	putchar('\n');
 	return (0);
 }
