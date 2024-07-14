4c9b3cfa0b7054367c9e844d8c72b92dd6bce7ee
Samuel Monye <toptechbro@yahoo.com>
2024-07-14 03:04:27 -0700

    Add: task 10 files

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * add - Adds two integers and returns the result
+ * @a: First integer to add
+ * @b:  Second integer to add
+ *
+ * Return: Result of the addition.
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
